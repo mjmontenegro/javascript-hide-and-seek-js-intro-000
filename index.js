f57b18773775c5bccf5217aa6840d689efb47abb
Mauricio Javier Montenegro <mjmontenegro@gmail.com>
2019-03-17 03:23:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ function increaseRankBy(n) {
   const lis = document.querySelectorAll('.ranked-list li');
   //Array.prototype.forEach.call(lis, (e) => e.innerHTML = `${parseInt(e.innerHTML) + 1}`);
   for(let i = 0; i < lis.length; i += 1) {
-    lis[i].innerHTML = parseInt(lis[i].innerHTML) + 1;
+    lis[i].innerHTML = parseInt(lis[i].innerHTML) + n;
   }
 }
