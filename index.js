6f667cb328232653511337690402ade2a08d4933
Mauricio Javier Montenegro <mjmontenegro@gmail.com>
2019-03-17 03:32:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,3 +9,15 @@ function increaseRankBy(n) {
   const lis = document.querySelectorAll('.ranked-list li');
   Array.prototype.forEach.call(lis, (e) => e.innerHTML = parseInt(e.innerHTML) + n);
 }
+function deepestChild() {
+  const grandNode = document.querySelector('div#grand-node');
+  let current = grandNode;
+  let next = grandNode.querySelector('*');
+  while (next != null) {
+    current = next;
+    next = next.querySelector('*');
+  }
+  return current;
+
+
+}
